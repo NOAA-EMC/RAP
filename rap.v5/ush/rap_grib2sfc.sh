06dd588703ff432d22289b51a1789ee2bbf93102
Benjamin.Blake <Benjamin.Blake@noaa.gov>
2020-04-08 19:27:37 +0000

    Final changes to match what NCO has
    Former-commit-id: f624e6bd977155af269856bcc9a94c1fdcf6377f

--- rap.v5/ush/rap_grib2sfc.sh
+++ rap.v5/ush/rap_grib2sfc.sh
@@ -43,50 +43,50 @@ set -A wrftwo_files VIS:surfac          \
 #                    CSNOW:surface
 
 
-wgrib2 -s ${filein} | grep 'REFC:entire atmosphere'  | wgrib2 -i ${filein} -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'MSLMA:mean sea level'    | wgrib2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'REFC:entire atmosphere'  | $WGRIB2 -i ${filein} -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'MSLMA:mean sea level'    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
 
 for field in ${wrftwo_files[@]}; do
-  wgrib2 -s ${filein} | grep ${field}    | wgrib2 -i ${filein} -append -grib_out ${fileout}
+  $WGRIB2 -s ${filein} | grep ${field}    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
 done
 
 #surfacelist="CICEP CFRZR CRAIN SFCR FRICVW SHTFL LHTFL GFLUX VGTYP SOTYP DSWRF DLWRF USWRF ULWRF CFNSF VBDSF VDDSF HPBL LTNG LAND HPBL"
 #
 #for field in ${surfacelist}; do
-#  wgrib2 -s ${filein} | grep ${field}:surface    | wgrib2 -i ${filein} -append -grib_out ${fileout}
+#  $WGRIB2 -s ${filein} | grep ${field}:surface    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
 #done
 
-#wgrib2 -s ${filein} | grep 'TSOIL:0-0 m below ground'    | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'TMP:2 m above ground'    | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'SPFH:2 m above ground'    | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'DPT:2 m above ground'    | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'MASSDEN:8 m above ground'    | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'UGRD:10 m above ground'  | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'VGRD:10 m above ground'  | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'HGT:lowest level of the wet bulb zero' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'PWAT:entire atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'AOTK:entire atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'COLMD:entire atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'TCOLWold:entire atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'TCOLIold:entire atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'LCDC:low cloud layer' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'MCDC:middle cloud layer' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'HCDC:high cloud layer' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'TCDC:entire atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'HGT:cloud ceiling' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'HGT:cloud base' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'PRES:cloud base' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'PRES:cloud top' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'HGT:cloud top' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'TMP:cloud top' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'ULWRF:top of atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'USWRF:top of atmosphere' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'PRES:tropopause' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'POT:tropopause' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'UGRD:tropopause' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep 'VGRD:tropopause' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-#wgrib2 -s ${filein} | grep '4LFTX:180-0 mb above ground' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'CAPE:255-0 mb above ground' | wgrib2 -i ${filein} -append -grib_out ${fileout}
-wgrib2 -s ${filein} | grep 'CIN:255-0 mb above ground' | wgrib2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'TSOIL:0-0 m below ground'    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'TMP:2 m above ground'    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'SPFH:2 m above ground'    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'DPT:2 m above ground'    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'MASSDEN:8 m above ground'    | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'UGRD:10 m above ground'  | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'VGRD:10 m above ground'  | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'HGT:lowest level of the wet bulb zero' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'PWAT:entire atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'AOTK:entire atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'COLMD:entire atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'TCOLWold:entire atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'TCOLIold:entire atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'LCDC:low cloud layer' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'MCDC:middle cloud layer' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'HCDC:high cloud layer' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'TCDC:entire atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'HGT:cloud ceiling' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'HGT:cloud base' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'PRES:cloud base' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'PRES:cloud top' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'HGT:cloud top' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'TMP:cloud top' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'ULWRF:top of atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'USWRF:top of atmosphere' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'PRES:tropopause' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'POT:tropopause' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'UGRD:tropopause' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep 'VGRD:tropopause' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+#$WGRIB2 -s ${filein} | grep '4LFTX:180-0 mb above ground' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'CAPE:255-0 mb above ground' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
+$WGRIB2 -s ${filein} | grep 'CIN:255-0 mb above ground' | $WGRIB2 -i ${filein} -append -grib_out ${fileout}
 
 exit 0
